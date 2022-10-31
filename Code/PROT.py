10c6307c1ca107c51fbdbfdd30c23f77e2260876
Joe Lin <102379756+joe-sclin@users.noreply.github.com>
2022-10-31 22:54:07 +0100

    PROT completed

---
+++ Code/PROT.py
@@ -0,0 +1,38 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('s', type=str)
+
+codon = {
+    "UUU": "F", "CUU": "L", "AUU": "I", "GUU": "V",
+    "UUC": "F", "CUC": "L", "AUC": "I", "GUC": "V",
+    "UUA": "L", "CUA": "L", "AUA": "I", "GUA": "V",
+    "UUG": "L", "CUG": "L", "AUG": "M", "GUG": "V",
+    "UCU": "S", "CCU": "P", "ACU": "T", "GCU": "A",
+    "UCC": "S", "CCC": "P", "ACC": "T", "GCC": "A",
+    "UCA": "S", "CCA": "P", "ACA": "T", "GCA": "A",
+    "UCG": "S", "CCG": "P", "ACG": "T", "GCG": "A",
+    "UAU": "Y", "CAU": "H", "AAU": "N", "GAU": "D",
+    "UAC": "Y", "CAC": "H", "AAC": "N", "GAC": "D",
+    "UAA": "X", "CAA": "Q", "AAA": "K", "GAA": "E",
+    "UAG": "X", "CAG": "Q", "AAG": "K", "GAG": "E",
+    "UGU": "C", "CGU": "R", "AGU": "S", "GGU": "G",
+    "UGC": "C", "CGC": "R", "AGC": "S", "GGC": "G",
+    "UGA": "X", "CGA": "R", "AGA": "R", "GGA": "G",
+    "UGG": "W", "CGG": "R", "AGG": "R", "GGG": "G"
+}
+
+prot = ''
+with open(parser.parse_args().s) as f:
+    while True:
+        s = f.read(3)
+        if codon[s] == 'X':
+            break
+        if len(s) < 3:
+            break
+        prot += codon[s]
+print(prot)
+
+# Codon dict to encode mRNA codon into amino acid sequences
+# mRNA sequence in text file is read every 3 characters
+# since storing full mRNA is unnecessary and consumed too much memory
